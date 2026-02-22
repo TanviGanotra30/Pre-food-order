e76f96fdc2ad0b33ac2f787db6ee6e8cf8ff97f2
Tanvi Ganotra <151958302+TanviGanotra30@users.noreply.github.com>
2026-02-22 20:27:34 +0530

    Edited menu and orders

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,7 +1,56 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import api from "../services/api"
+import "../index.css"
 import "./menu.css"
+import idli from "../assets/foods/idli.png"
+import hakka from "/src/assets/foods/hakka-noodles.png"
+import burger from "../assets/foods/Burger.png"
+import chaap from "../assets/foods/masala-chaap.png"
+import paneer from "../assets/foods/paneer-tikka.png"
+import honey from "../assets/foods/honey-chilli.png"
+import dosa from "../assets/foods/masala-dosa.png"
+import momos from "../assets/foods/steam-momos.png"
+import fries from "../assets/foods/peri-peri-fries.png"
+import chana from "../assets/foods/chana-samosa.png"
+import chole from "../assets/foods/chole-bhautre.png"
+import pav from "../assets/foods/pav-bhaji.png"
+import sandwich from "../assets/foods/grilled-sandwich.png"
+import pizza from "../assets/foods/pizza.png"
+import kurkure from "../assets/foods/kurkure-momos.png"
+import jalebi from "../assets/foods/jalebi.png"
+import mazza from "../assets/foods/mazza.png"
+import placeholder from "../assets/foods/placeholder.png"
+
+const normalize = (str) =>
+  str
+    .toLowerCase()
+    .replace(/\(.*?\)/g, "")   
+    .replace(/[-_]/g, " ")     
+    .replace(/\s+/g, " ")   
+    .trim()
+
+const foodImages = {
+  "idli": idli,
+  "hakka noodles": hakka,
+  "burger": burger,
+  "masala chaap": chaap,
+  "paneer tikka": paneer,
+  "honey chilly": honey,
+  "masala dosa": dosa,
+  "steam momos": momos,
+  "peri peri fries": fries,
+  "chana samosa": chana,
+  "chole bhature": chole,
+  "pav bhaji": pav,
+  "grilled sandwich": sandwich,
+  "pizza": pizza,
+  "kurkure momos": kurkure,
+  "jalebi": jalebi,
+  "mazza": mazza,
+  
+}
+
 
 export default function Menu() {
   const [menu, setMenu] = useState([])
@@ -63,7 +112,7 @@ export default function Menu() {
             <div className={`food-card ${disabled ? "disabled" : ""}`} key={item.item_id}>
               
               <div className="food-img">
-                <img src="/food-placeholder.png" alt={item.name} />
+                <img src={foodImages[normalize(item.name)] ?? placeholder} alt={item.name}/>
               </div>
 
               <h3>{item.name}</h3>
